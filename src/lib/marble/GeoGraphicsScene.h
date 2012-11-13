1b39e958d5cdc713f5b13aaa935a0a0243189585
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2012-11-13 16:46:26 +0100

    introduce GeoGraphicsScene::GlTile and use glDrawElements

--- src/lib/marble/GeoGraphicsScene.h
+++ src/lib/marble/GeoGraphicsScene.h
@@ -14,6 +14,7 @@
 
 #include "marble_export.h"
 #include "MarbleGlobal.h"
+#include "TileId.h"
 
 #include <QObject>
 #include <QList>
@@ -76,6 +77,10 @@ public:
      * @return The list of items in the specified box in no specific order.
      */
     QList<GeoGraphicsItem *> items( const GeoDataLatLonBox &box, int maxZoomLevel ) const;
+
+    QList<GeoGraphicsItem *> tile( const TileId &id ) const;
+
+    QList<TileId> tiles( const GeoDataLatLonBox &box, int maxZoomLevel ) const;
 
 private:
     GeoGraphicsScenePrivate * const d;
